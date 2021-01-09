1fcdf3916201820f32bbf585e8dd2736160b387d
Yousra Miloudi <miloudi112@hotmail.com>
2021-01-09 12:12:45 +0100

    Structure composants

---
+++ checknchill/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+
+ 
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
