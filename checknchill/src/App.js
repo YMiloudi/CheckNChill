5c6dbdd373616e247d6e504410cdad6506317067
Yousra Miloudi <miloudi112@hotmail.com>
2021-03-10 18:52:44 +0100

    lottie and main page

--- checknchill/src/App.js
+++ checknchill/src/App.js
@@ -1,47 +1,22 @@
 import React from 'react';
-import Weather from './components/weather/Weather';
 import './App.css';
-import Quotes from './components/dontforget/Quotes';
-import MoviesList from './components/movies/MoviesList';
-
+import Home from './components/Home/Home'
 
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-
-    this.state={
-      classImg: ""
-    }
-  }
-
-  // Création de la fonction qui va récup la donnée et à laquelle on va assigner une condition pour afficher une img
-  getImgByTemp(temp){
-    if(temp > 16){
-      this.setState({classImg: "imgHot"})
-    }else{
-      this.setState({classImg: "imgCold"})
-    }
-  }
 
   render(){
     return (
-      <div className={this.state.classImg}> {/*Création du state classImg qui définit la div temp*/}
-        
-        <Weather onDataSearch={(temp)=>this.getImgByTemp(temp)}/> {/*onDataSearchs => Fonction qui va récup la data img*/}
-
-        <Quotes/>
-
-        <MoviesList/>
-        
+      <div> 
+        <Home/>  
       </div>
 
     );
   }
 }
 
-//
+
 
 
 
