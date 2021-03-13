dc5d42e625a56fadb19010b5e050bf5d4ff987ed
Nathalie Crispin <n.crispin.digital@gmail.com>
2021-03-13 11:51:19 +0100

    movie box css

--- checknchill/src/components/movies/Movie.js
+++ checknchill/src/components/movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './Movie.css';
 
+import Poster from "../../assets/ovh.png"
+
 class Movie extends Component{
     
     constructor (props){
@@ -11,7 +13,7 @@ class Movie extends Component{
         return(
             <div className='effect'>
                 <div className='transition'>
-                    <img src={this.props.movie.poster || 'https://pictures.betaseries.com/films/affiches/original/68126.jpg'} alt={this.props.movie.title} className='imageSlide'/>
+                    <img src={Poster} />
                     <div className='overlay'>
                         <p className='text'>
                             <h4 className='titleImage'>{this.props.movie.title}</h4>
